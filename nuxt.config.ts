25cb1e29d383ea58fefa1cf79a6a9f6c64a6b3cf
Sergey <anfalov@camini-pr.ru>
2023-04-04 12:08:25 +0500

    добавили store применяя pinia, добавили три товара в массив products... определились как указывать мета теги для страниц, приступаем к переносу имеющихся материалов

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,14 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
     pages: true,
+    modules: [
+        [
+            '@pinia/nuxt',
+            {
+                autoImports: [
+                    ['defineStore', 'definePiniaStore'],
+                ]
+            }
+        ]
+    ]
 })
--- pages/index.vue
+++ pages/index.vue
@@ -1,15 +1,29 @@
 <template>
     <div>
+        <Head>
+            <Title>Unidrum by Gleb Rudakov</Title>
+            <Meta name="Музыкальный инструмент unidrum" content="Создаю музыкальные инструменты"/>
+        </Head>
         INDEX PAGE
         <!--  -->
         <NuxtLink to="/about">Об инструменте</NuxtLink>
     </div>
+    {{ products }}
 </template>
 
-<script>
-    export default {
-        name: 'index'
-    }
+<script setup>
+
+
+
+    import { useProductsStore } from '../stores/products'
+
+    const productsStore = useProductsStore()
+
+    const products = computed(() => {
+        return productsStore.products
+    })
+    
+
 </script>
 
 <style scoped>
